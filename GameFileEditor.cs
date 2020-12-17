eeee491264b35b26c20b1228a4ba31f8156843f4
Kye <36981621+kyeondiscord@users.noreply.github.com>
2020-12-17 16:34:22 +1100

    Add files via upload

---
+++ GameFileEditor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ProSwapperFOV.Properties;
+namespace ProSwapperFOV
+{
+    public static class GameFileEditor
+    {
+        public static string fndir
+        {
+            get
+            {
+                string defaultpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\FortniteGame\Saved\Config\WindowsClient\GameUserSettings.ini";
+                if (File.Exists(defaultpath))
+                {
+                    return defaultpath;
+                }
+                else
+                {
+                    using (OpenFileDialog o = new OpenFileDialog())
+                    {
+                        o.Title = "Select your Fortnite GameUserSettings.ini";
+                        o.Filter = "GameUserSettings.ini (*.ini*)|*.ini";
+                        o.ShowDialog();
+
+                        if (o.ShowDialog() == DialogResult.OK)
+                        {
+                            return o.FileName;
+                        }
+                        return "null";
+                    }
+                }
+            }
+        }
+        private static string GameFile { get; set; }
+        public static void SetRes(string YRes, string XRes)
+        {
+            try
+            {
+                GameFile = string.Empty;
+                string line;
+                using (StreamReader file = new StreamReader(Settings.Default.GameDir))
+                {
+                    while ((line = file.ReadLine()) != null)//Reads line one by one
+                    {
+                        if (line.StartsWith("LastConfirmedFullscreenMode="))
+                        {
+                            GameFile += "LastConfirmedFullscreenMode=2" + Environment.NewLine;
+                            continue;
+                        }
+                        if (line.StartsWith("PreferredFullscreenMode="))
+                        {
+                            GameFile += "PreferredFullscreenMode=2" + Environment.NewLine;
+                            continue;
+                        }
+                        if (line.StartsWith("LastConfirmedFullscreenMode="))
+                        {
+                            GameFile += "FullscreenMode=2" + Environment.NewLine;
+                            continue;
+                        }
+                        //X Res
+                        if (line.StartsWith("ResolutionSizeX="))
+                        {
+                            GameFile += "ResolutionSizeX=" + XRes + Environment.NewLine;
+                            continue;
+                        }
+                        if (line.StartsWith("LastUserConfirmedResolutionSizeX="))
+                        {
+                            GameFile += "LastUserConfirmedResolutionSizeX=" + XRes + Environment.NewLine;
+                            continue;
+                        }
+
+                        //Y Res
+                        if (line.StartsWith("ResolutionSizeY="))
+                        {
+                            GameFile += "ResolutionSizeY=" + YRes + Environment.NewLine;
+                            continue;
+                        }
+                        if (line.StartsWith("LastUserConfirmedResolutionSizeY="))
+                        {
+                            GameFile += "LastUserConfirmedResolutionSizeY=" + YRes + Environment.NewLine;
+                            continue;
+                        }
+                        GameFile += line + Environment.NewLine;
+                    }
+                }
+                    File.WriteAllText(Settings.Default.GameDir, GameFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pro Swapper FOV Changer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.IO;
+using ProSwapperFOV.Properties;
+namespace ProSwapperFOV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Get's user's GameUserSettings.ini
+            if (!File.Exists(Settings.Default.GameDir))
+            {
+                Settings.Default.GameDir = GameFileEditor.fndir;
+                Settings.Default.Save();
+            }
+            fndir.Text = Settings.Default.GameDir;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+        private void FOVSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            valuelbl.Content = "Value: " + (int)FOVSlider.Value;
+        }
+
+        private void setfov_Click(object sender, RoutedEventArgs e)
+        {
+            int fovval = (int)FOVSlider.Value;
+            GameFileEditor.SetRes(fovval.ToString(), "1920");
+            MessageBox.Show("Set FOV to " + fovval, "Pro Swapper FOV", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void setresbutton_Click(object sender, RoutedEventArgs e)
+        {
+            GameFileEditor.SetRes(xres.Text, yres.Text);
+            MessageBox.Show("Set Resolution to " + xres.Text + " x " + yres.Text, "Pro Swapper Resolution Changer", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
